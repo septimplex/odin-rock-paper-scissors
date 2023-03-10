f512f8be014a2e8b63465ba557a3aada94dddcd2
Septimplex <septimplexx@gmail.com>
2023-03-10 22:40:09 +0100

    add game function, best of 5

--- javascript.js
+++ javascript.js
@@ -6,24 +6,75 @@ function getComputerChoice(){
     return choices[computerChoice];
 }
 
+//player input
+function playerInput(){
+    let player = prompt("Rock, paper or scissors?");
+    return player;
+
+}
+
 
 
 function playRound(playerSelection, computerSelection){
     let playerSelectionLower = playerSelection.toLowerCase();
     if (playerSelectionLower == computerSelection){
-        return "It's a tie.";
+        /*return "It's a tie.";*/
+        return 0;
     }
     else if((playerSelectionLower == "paper" && computerSelection == "rock") || (playerSelectionLower == "rock" && computerSelection == "scissors") || (playerSelectionLower == "scissors" && computerSelection == "paper")){
-        return "You win! " + playerSelectionLower + " beats " + computerSelection + ".";
+        /*return "You win! " + playerSelectionLower + " beats " + computerSelection + ".";*/
+        return 1;
+    }   
+    else{
+        /*return "You lose! " + computerSelection + " beats " + playerSelectionLower + ".";*/
+        return -1;
+    }
+}
+
+
+function game(){
+    let computerScore = 0;
+    let playerScore = 0;
+    let current = 0;
+
+    for(let i = 0; i < 5; i++){
+        let player = playerInput();
+        let computer = getComputerChoice();
+
+        current = playRound(player,computer); // returns 0, 1 or -1;
+        // tied
+        if (current == 0){
+            console.log("It's a tie.");
+        }
+        // win for player
+        else if  (current == 1){
+            console.log("You win! " + player + " beats " + computer + ".");
+            playerScore += 1;
+        }
+        else{
+            console.log("You lose! " + computer + " beats " + player + ".");
+            computerScore +=1;
+        }   
+    }
+
+    console.log("GAME OVER");
+
+    if(computerScore == playerScore){
+        console.log("Game was tied!");
+    }
+    else if(playerScore > computerScore){
+        console.log("Congratulations, you won!");
     }
     else{
-        return "You lose! " + computerSelection + " beats " + playerSelectionLower + ".";
+        console.log("You lost... Better luck next time!");
     }
 
 }
 
+game();
+/*
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+console.log(playRound(playerSelection, computerSelection));*/
 
