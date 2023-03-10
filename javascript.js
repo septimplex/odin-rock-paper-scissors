0d09e3d9470658efdc153d54bd925691b160c82c
Septimplex <septimplexx@gmail.com>
2023-03-10 22:01:45 +0100

    Add function for single round of game

--- javascript.js
+++ javascript.js
@@ -6,4 +6,24 @@ function getComputerChoice(){
     return choices[computerChoice];
 }
 
-console.log(getComputerChoice());
+
+
+function playRound(playerSelection, computerSelection){
+    let playerSelectionLower = playerSelection.toLowerCase();
+    if (playerSelectionLower == computerSelection){
+        return "It's a tie.";
+    }
+    else if((playerSelectionLower == "paper" && computerSelection == "rock") || (playerSelectionLower == "rock" && computerSelection == "scissors") || (playerSelectionLower == "scissors" && computerSelection == "paper")){
+        return "You win! " + playerSelectionLower + " beats " + computerSelection + ".";
+    }
+    else{
+        return "You lose! " + computerSelection + " beats " + playerSelectionLower + ".";
+    }
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
+
